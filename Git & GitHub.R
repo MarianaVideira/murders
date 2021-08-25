cbc24af11e4cbd109e1b187aae8fe5f04d9fdf8e
Mariana Videira <mariana.videira@gmail.com>
2021-08-25 13:30:58 +0100

    Commit all files

---
+++ Git & GitHub.R
@@ -0,0 +1,19 @@
+#Why Use Git? 
+
+# Reasons to use Git and GitHub:
+
+  # Version-control: Permits us to keep track of changes we made to code, to revert 
+    # back to previous versions of files, to test ideas using new branches and 
+    # decide if we want to merge to the original.
+
+  # Collaboration: On a centralized repo, multiple people may make changes to the 
+    #code and keep versions synced. A pull request allows anyone to suggest 
+    # changes to your code.
+
+  # Sharing code
+
+
+# A Git project can be thought of as having three parts:
+  # A Working Directory: where you’ll be doing all the work: creating, editing, deleting and organizing files
+  # A Staging Area: where you’ll list changes you make to the working directory
+  # A Repository: where Git permanently stores those changes as different versions of the project
